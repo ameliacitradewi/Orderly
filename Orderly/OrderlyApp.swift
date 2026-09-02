d5962016f717cf73e2556c29e3e03fb2e0e7f139
Amelia Citra <ameliacitradewi@gmail.com>
2026-09-02 16:30:05 +0700

    Initial Commit

---
+++ Orderly/OrderlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderlyApp.swift
+//  Orderly
+//
+//  Created by Amelia Citra on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct OrderlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
